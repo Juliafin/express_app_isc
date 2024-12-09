3124a259666fe3b940ca3de36a1266a7186c422e
Julia Finarovsky <juliafinarovsky@gmail.com>
2024-12-09 02:06:10 -0500

    Setup jest and unit tests

---
+++ src/test/server.test.ts
@@ -0,0 +1,14 @@
+import { createServer } from "../server";
+import { Server } from 'http';
+
+describe('createServer', () => {
+    let server: any;
+    afterAll((done) => {
+        server.close(done);
+    });
+
+    it('should return an instance of an express server', () => {
+        server = createServer();
+        expect(server).toBeInstanceOf(Server);
+    })
+})
